96a27e1cf64e4c66f0e07df806965919fe05b757
Giovani Santos Cardoso <gioos.cardoso@gamil.com>
2023-01-31 15:19:33 -0300

    CRUD criado com ASP.NET Core 7

---
+++ AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
@@ -0,0 +1,12 @@
+﻿namespace AwesomeDevEvents.API.Entities
+{
+    public class DevEventSpeaker
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string TalkTitle { get; set; }
+        public string TalkDescription { get; set; } = "";
+        public string LinkedInProfile { get; set; } = "";
+
+    }
+}
